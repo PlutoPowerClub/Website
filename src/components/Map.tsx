698d31419274af37cbf75c6b625119b6280196dc
jones58 <108466184+jones58@users.noreply.github.com>
2024-07-01 20:12:41 +0100

    add static assets folder

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 import React, { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
-import geojson from "../geoJSON";
 
-// Make sure to replace 'your_mapbox_access_token' with your actual Mapbox access token
+// Replace 'your_mapbox_access_token' with your actual Mapbox access token
 mapboxgl.accessToken =
   "pk.eyJ1Ijoiam9uZXM1ODEiLCJhIjoiY2xwNzM4Y3JpMXZ1NjJrcWswNDFrbnl1ZiJ9.Ud2Oqbe9kgEmB3U3UOH98w";
+
 const MapboxMap: React.FC = () => {
   const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
@@ -17,6 +15,35 @@ const MapboxMap: React.FC = () => {
         style: "mapbox://styles/mapbox/streets-v11", // Style of the map
         center: [-1.2982167, 50.7029], // Initial map center in [lng, lat]
         zoom: 18, // Initial zoom level
+      });
+
+      map.on("load", () => {
+        // Load GeoJSON file
+        fetch("src/components/buildings.geojson")
+          .then((response) => response.json())
+          .then((data) => {
+            // Add GeoJSON data as a source
+            map.addSource("custom-data", {
+              type: "geojson",
+              data: data,
+            });
+
+            // Add a layer to display the building GeoJSON data as 3D extrusions
+            map.addLayer({
+              id: "custom-layer",
+              type: "fill-extrusion",
+              source: "custom-data",
+              paint: {
+                "fill-extrusion-color": "#ffffff", // Color of the building
+                "fill-extrusion-height": 50, // Height of the building
+                "fill-extrusion-base": 0, // Base height of the building
+                "fill-extrusion-opacity": 0.6, // Opacity of the building
+              },
+            });
+          })
+          .catch((error) => {
+            console.error("Error loading GeoJSON:", error);
+          });
       });
 
       // Clean up on unmount
