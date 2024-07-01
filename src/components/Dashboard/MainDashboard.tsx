52f5947f35a048bb5ac0527ea38a7aef2ae5011a
jones58 <108466184+jones58@users.noreply.github.com>
2024-07-01 17:38:33 +0100

    edit main dashboard components

--- src/components/Dashboard/MainDashboard.tsx
+++ src/components/Dashboard/MainDashboard.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import ChartThree from "../Charts/HouseholdEnergy";
 import ChartFour from "../Charts/CommunityEnergy";
 import WeatherForecast from "../WeatherForecast";
+import InviteNeighbours from "../Invite Neighbours";
 
 const MainDashboard: React.FC = () => {
   return (
     <>
-      <WeatherForecast />
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        <WeatherForecast />
+        <InviteNeighbours />
         <ChartThree />
         <ChartFour />
       </div>
