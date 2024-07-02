eb8f7a972078a27a5f6132221f85823a6980e5ad
jones58 <108466184+jones58@users.noreply.github.com>
2024-07-02 12:50:21 +0100

    change neighbour

--- src/components/Invite Neighbours.tsx
+++ src/components/Invite Neighbours.tsx
@@ -5,11 +5,16 @@ const InviteNeighbours = () => {
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-5">
       <h5 className="text-xl font-semibold text-black dark:text-white">
-        Add Solar Neighbours to increase your community share!
+        Add Solar Neighbours
       </h5>
-      <div className=" flex justify-end">
+      <p>
+        {" "}
+        Increase your community's solar impact by adding neighbours to the solar
+        map
+      </p>
+      <div className="flex">
         <Link href="/invites">
-          <button className="rounded-md bg-blue-600 px-4 py-2 text-white transition duration-150 ease-in-out hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+          <button className="text-bold my-2 rounded-md bg-[#7cc45f] px-4 py-1 text-lg text-black transition duration-150 ease-in-out hover:bg-opacity-70 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
             See Community Impact
           </button>
         </Link>
