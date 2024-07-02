468606b4a31e57eb5c062b7c1072986fc0d6ef22
jones58 <108466184+jones58@users.noreply.github.com>
2024-07-02 14:12:10 +0100

    edit page to remove metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
+"use client";
 import MainDashboard from "@/components/Dashboard/MainDashboard";
-import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-
-export const metadata: Metadata = {
-  title: "Starfish Energy",
-  description: "Starfish Energy wants you to jo",
-};
 
 export default function Home() {
   return (
