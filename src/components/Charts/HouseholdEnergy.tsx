5c983b62d1672e1c5607aad46cab302e4223e232
jones58 <108466184+jones58@users.noreply.github.com>
2024-10-09 16:06:02 +0100

    change text sizes of titles

--- src/components/Charts/HouseholdEnergy.tsx
+++ src/components/Charts/HouseholdEnergy.tsx
@@ -54,9 +54,9 @@ const ChartThree: React.FC = () => {
 
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-5">
-      <div className="mb-3 justify-between gap-4 sm:flex">
+      <div className="mb-4 justify-between gap-4 sm:flex">
         <div>
-          <h5 className="text-xl font-semibold text-black dark:text-white">
+          <h5 className="flex items-center justify-between text-3xl font-semibold text-black dark:text-white">
             Your Household Energy
           </h5>
         </div>
