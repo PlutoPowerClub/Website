ade2fe9f4c3474eba97b6ed892415697901797f9
jones58 <108466184+jones58@users.noreply.github.com>
2024-10-09 18:09:45 +0100

    rename dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
-import ChartThree from "../Charts/HouseholdEnergy";
-import ChartFour from "../Charts/CommunityEnergy";
-import WeatherForecast from "../WeatherForecast";
-import InviteNeighbours from "../Invite Neighbours";
-import Cash from "../Cash";
+import ChartThree from "./Charts/HouseholdEnergy";
+import ChartFour from "./Charts/CommunityEnergy";
+import WeatherForecast from "./WeatherForecast";
+import InviteNeighbours from "./Invite Neighbours";
+import Cash from "./Cash";
 
 const option1 = 30;
 const MainDashboard: React.FC = () => {
