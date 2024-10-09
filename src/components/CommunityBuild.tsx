fb7c4a293d98b368b8d70bfe32c3eb1f519b2ddf
jones58 <108466184+jones58@users.noreply.github.com>
2024-10-09 17:00:25 +0100

    improve display of community page and add icons through tailwind and iconify

--- src/components/CommunityBuild.tsx
+++ src/components/CommunityBuild.tsx
@@ -2,7 +2,7 @@ const CommunityBuild = () => {
   return (
     <div className=" mt-0 rounded-sm border border-stroke bg-white bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 ">
       <h1 className="mb-4 text-3xl font-bold text-black">Build a Community</h1>
-      <p className="text-gray-700 text-lg lg:text-xl">
+      <p className="text-lg text-black lg:text-xl">
         Tap on the map to add a neighbour's house to the solar map and see your
         community's energy use change.
       </p>
--- src/components/CommunityShare.tsx
+++ src/components/CommunityShare.tsx
@@ -4,30 +4,35 @@ const CommunityBuild = () => {
   return (
     <div className=" mt-0 rounded-sm border border-stroke bg-white bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 ">
       <h1 className="mb-4 text-3xl font-bold text-black">Share</h1>
-      <p className="text-gray-700 text-lg lg:text-xl">Share these results:</p>
-      <div className="">
-        <Link
-          href={`https://twitter.com/intent/tweet?text=&url=`}
-          title="Share on Twitter"
-        >
-          <span className="icon-[mdi--twitter]"></span>
-        </Link>
-        <Link href={`https://wa.me/?text=`} title="Share on WhatsApp">
-          <span className="icon-[mdi--whatsapp]"></span>
-        </Link>
-        <Link
-          href={`https://www.facebook.com/sharer/sharer.php?u=`}
-          title="Share on Facebook"
-        >
-          <span className="icon-[mdi--facebook]"></span>
-        </Link>
-        <Link
-          href={`mailto:?subject=&body=Check out this article:
+      <div className="flex flex-row justify-normal gap-10">
+        <p className="text-xl text-black">Share these results:</p>
+        <div className="flex flex-row justify-between space-x-5 text-2xl">
+          <Link
+            href={`https://twitter.com/intent/tweet?text=&url=https://plutowebsite.vercel.app/`}
+            title="Share on Twitter"
+          >
+            <span className="icon-[mdi--twitter] hover:text-black"></span>
+          </Link>
+          <Link
+            href={`https://wa.me/?text=https://plutowebsite.vercel.app/`}
+            title="Share on WhatsApp"
+          >
+            <span className="icon-[mdi--whatsapp] hover:text-black"></span>
+          </Link>
+          <Link
+            href={`https://www.facebook.com/sharer/sharer.php?u=https://plutowebsite.vercel.app/`}
+            title="Share on Facebook"
+          >
+            <span className="icon-[mdi--facebook] hover:text-black"></span>
+          </Link>
+          <Link
+            href={`mailto:?subject=&body=Check out this site:https://plutowebsite.vercel.app/
           )}`}
-          title="Share via Email"
-        >
-          <span className="icon-[mdi--email]"></span>
-        </Link>
+            title="Share via Email"
+          >
+            <span className="icon-[mdi--email] hover:text-black"></span>
+          </Link>
+        </div>
       </div>
     </div>
   );
