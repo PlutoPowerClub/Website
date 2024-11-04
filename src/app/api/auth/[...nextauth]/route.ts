96b2bc287cb0e076d7fae36623b784c602852112
jones58 <108466184+jones58@users.noreply.github.com>
2024-11-04 15:26:51 +0000

    add proper nextauth cognito setup

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,19 @@
 import NextAuth from "next-auth";
 import CognitoProvider from "next-auth/providers/cognito";
+import process from "process";
 
 export const authOptions = {
   providers: [
     CognitoProvider({
-      clientId: process.env.COGNITO_CLIENT_ID ?? "",
-      clientSecret: process.env.COGNITO_CLIENT_SECRET ?? "",
+      clientId: process.env.COGNITO_CLIENT_ID as string,
+      clientSecret: process.env.COGNITO_CLIENT_SECRET as string,
       issuer: process.env.COGNITO_DOMAIN,
+      checks: "nonce",
     }),
   ],
+  debug: true,
 };
 
 export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
