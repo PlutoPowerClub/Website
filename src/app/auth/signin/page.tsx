936138c2e121d0813a429ae77b9e19ae7f080513
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-06 14:03:39 +0000

    improve desktop look

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -9,45 +9,48 @@ export const metadata: Metadata = {
 
 const SignIn: React.FC = () => {
   return (
-    <div className="flex items-center justify-center text-neutral-800">
-      <div className="xl:bg border-stroke w-full rounded-2xl border bg-red-50 shadow-lg">
-        <div className="flex items-center justify-center sm:py-5 xl:justify-start xl:px-5">
-          <div className="w-full p-6 py-5 md:max-w-xl xl:p-8">
-            <p className="py-5 text-2xl font-bold sm:mb-12 sm:text-5xl">
-              Sign in to visit Pluto.
-            </p>
-            <Form />
-            <div className="mt-4 rounded-xl bg-neutral-50 p-4 sm:mt-12">
-              <h2 className="mb-1 text-xl font-semibold text-neutral-800 sm:text-2xl">
-                Demo Account
-              </h2>
-              <div className="space-y-2">
-                <p className="text-body sm:text-xl">
-                  Email:
-                  <br />
-                  <span className="font-medium text-neutral-800">
-                    jackkershaw@protonmail.com
-                  </span>
-                </p>
-                <p className="text-body sm:text-xl">
-                  Password:
-                  <br />
-                  <span className="font-medium text-neutral-800">
-                    Test1!Test1!
-                  </span>
-                </p>
+    <div className="flex max-h-screen items-center justify-center text-neutral-800">
+      <div className="border-stroke w-full rounded-2xl border bg-red-50 shadow-lg">
+        <div className="flex">
+          <div className="w-full flex-col justify-center lg:w-1/2 lg:px-10 lg:py-10">
+            <div className="w-full p-6 py-5 lg:max-w-xl ">
+              <p className="text-2xl font-bold tracking-tight sm:mb-10 sm:text-5xl xl:text-6xl">
+                Sign in to visit Pluto.
+              </p>
+              <Form />
+              <div className="mt-4 rounded-xl bg-neutral-50 p-4 lg:mt-8">
+                <h2 className="mb-1 text-xl font-semibold text-neutral-800 sm:text-2xl ">
+                  Demo Account
+                </h2>
+                <div className="space-y-2 lg:space-y-3">
+                  <p className="text-body sm:text-xl xl:text-2xl">
+                    Email:
+                    <br />
+                    <span className="font-medium text-neutral-800">
+                      jackkershaw@protonmail.com
+                    </span>
+                  </p>
+                  <p className="text-body sm:text-xl xl:text-2xl">
+                    Password:
+                    <br />
+                    <span className="font-medium text-neutral-800">
+                      Test1!Test1!
+                    </span>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-
-          <div className="hidden h-full min-h-[800px] items-center overflow-hidden rounded-3xl p-6 xl:flex">
-            <Image
-              src="/images/surface.png"
-              alt="Surface of Pluto."
-              height={800}
-              width={1200}
-              className="rounded-2xl object-cover grayscale"
-            />
+          <div className="hidden lg:block lg:w-1/2">
+            <div className="relative h-full w-full">
+              <Image
+                src="/images/surface.png"
+                alt="Surface of Pluto."
+                fill
+                className="rounded-xl rounded-l-none object-cover"
+                priority
+              />
+            </div>
           </div>
         </div>
       </div>
