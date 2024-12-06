f4ceef97b12c3544fc9cebf6a5c1312dbff44b5d
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-06 14:33:11 +0000

    add animation on page load

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,5 +8,15 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [addDynamicIconSelectors()],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+    },
+  },
 };
 export default config;
