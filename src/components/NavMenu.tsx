2724e0e2ef34445373a80cff0b5569d686fbee55
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-06 13:24:42 +0000

    make sign in/sign out button look like proper button

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -7,30 +7,29 @@ function AuthButton() {
 
   if (session) {
     return (
-      <>
-        <button className="text-xl sm:text-2xl" onClick={() => signOut()}>
-          {" "}
-          Sign out
-        </button>
-      </>
+      <button
+        className="rounded-lg bg-neutral-800 px-5 py-2 text-xl text-neutral-50 hover:bg-neutral-700 sm:text-2xl"
+        onClick={() => signOut()}
+      >
+        Sign out
+      </button>
     );
   }
   return (
-    <>
-      <>
-        <button className="text-xl sm:text-2xl" onClick={() => signIn()}>
-          Sign in
-        </button>
-      </>
-    </>
+    <button
+      className="rounded-lg bg-neutral-800 px-5 py-2 text-xl text-neutral-50 hover:bg-neutral-700 sm:text-2xl"
+      onClick={() => signIn()}
+    >
+      Sign in
+    </button>
   );
 }
 
 export default function NavMenu() {
   return (
-    <div className="flex flex-row justify-between bg-indigo-100 py-5 font-bold text-neutral-800 sm:py-10">
+    <div className="flex flex-row items-center justify-between bg-indigo-100 py-4 font-bold text-neutral-800 sm:py-6">
       <Link href="/">
-        <h1 className="text-3xl sm:text-4xl">Pluto </h1>
+        <h1 className="text-3xl sm:text-4xl">Pluto</h1>
       </Link>
       <AuthButton />
     </div>
