583220aee8413979a4e10904658a889628b0a8a7
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-06 15:59:05 +0000

    add tailwind forms

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [addDynamicIconSelectors()],
+  plugins: [addDynamicIconSelectors(), require("@tailwindcss/forms")],
   theme: {
     extend: {
       keyframes: {
