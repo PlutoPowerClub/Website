ed70aef0e9b0d19e7bf1eeba95b4afebf2edc0f8
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-17 17:11:43 +0000

    remove extra code and size iframe better on mobile

--- src/app/tips/page.tsx
+++ src/app/tips/page.tsx
@@ -12,12 +12,12 @@ export default function Home() {
         <h1 className="text-2xl font-bold text-neutral-800 sm:text-3xl">
           Energy Saving Guide
         </h1>
-        <p className="mb-8 max-w-xl text-xl text-neutral-800">
+        <p className="max-w-xl text-xl text-neutral-800">
           Discover smart ways to reduce your energy consumption and make a
           positive impact on the environment while benefitting your community.
         </p>
       </ComponentLayout>
-      <div className="relative aspect-video w-full rounded-xl shadow-lg sm:overflow-hidden">
+      <div className="relative h-[400px] w-full rounded-xl shadow-lg sm:aspect-video sm:h-auto sm:overflow-hidden">
         <iframe
           src="https://octopus.energy/blog/energy-saving-tips/"
           className="absolute left-0 top-0 h-full w-full border-0"
