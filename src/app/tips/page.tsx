22babbeec13cfa59bfa473ec68cddcf1e17d9b97
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-17 16:11:17 +0000

    improve tips page with component layout

--- src/app/tips/page.tsx
+++ src/app/tips/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-
+import ComponentLayout from "@/components/componentLayout";
 export const metadata: Metadata = {
   title: "Energy Tips",
   description: "Helpful energy tips to help you save energy.",
@@ -7,14 +7,16 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="mb-6 text-2xl font-bold text-neutral-800 md:text-3xl">
-        Energy Saving Guide
-      </h1>
-      <p className="mb-8 text-lg text-neutral-800">
-        Discover smart ways to reduce your energy consumption and make a
-        positive impact on the environment while benefitting your community.
-      </p>
+    <div className="flex flex-col space-y-4">
+      <ComponentLayout>
+        <h1 className="text-2xl font-bold text-neutral-800 sm:text-3xl">
+          Energy Saving Guide
+        </h1>
+        <p className="mb-8 max-w-xl text-xl text-neutral-800">
+          Discover smart ways to reduce your energy consumption and make a
+          positive impact on the environment while benefitting your community.
+        </p>
+      </ComponentLayout>
       <div className="relative aspect-video w-full rounded-xl shadow-lg sm:overflow-hidden">
         <iframe
           src="https://octopus.energy/blog/energy-saving-tips/"
