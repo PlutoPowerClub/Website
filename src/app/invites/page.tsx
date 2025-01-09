d7de834f4028b23af7e5c5b287f2b84c00a7b40a
jones58 <108466184+jones58@users.noreply.github.com>
2025-01-09 11:17:40 +0000

    improve mobile layout map page

--- src/app/invites/page.tsx
+++ src/app/invites/page.tsx
@@ -21,9 +21,14 @@ export default async function Home() {
   return (
     <>
       <div className="flex flex-col justify-between gap-2 lg:flex-row">
+        <div className="sm:hidden">
+          <CommunityBuild />
+        </div>
         <Map />
         <div className="flex flex-col gap-2">
-          <CommunityBuild />
+          <div className="hidden sm:block">
+            <CommunityBuild />
+          </div>
           <CommunityEnergyChart />
           <CommunityShare />
         </div>
