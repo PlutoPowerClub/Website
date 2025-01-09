cfbfab37b0508e039e2ad5e52f76c5e2ae94225a
jones58 <108466184+jones58@users.noreply.github.com>
2025-01-09 12:29:32 +0000

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="mt-5 w-full rounded-xl bg-neutral-50 p-6 shadow-lg">
+      <div className="mx-auto max-w-6xl">
+        <h3 className="text-xl font-semibold text-neutral-800">Quick Links</h3>
+        <ul className="mt-2 space-y-2">
+          <li>
+            <Link
+              href="/"
+              className="text-md text-neutral-800 hover:text-indigo-600"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/invites"
+              className="text-md text-neutral-800 hover:text-indigo-600"
+            >
+              Invite Neighbours
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/privacy-policy"
+              className="text-md text-neutral-800 hover:text-indigo-600"
+            >
+              Privacy Policy
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
