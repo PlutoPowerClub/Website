6e2ba79efe21dc34ff1f3eb56d6231f2039b736e
jones58 <108466184+jones58@users.noreply.github.com>
2025-01-17 10:50:00 +0000

    improve energy tips page on mobile including option to open in new tab

--- src/app/tips/page.tsx
+++ src/app/tips/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
 import ComponentLayout from "@/components/componentLayout";
+import Link from "next/link";
+
 export const metadata: Metadata = {
   title: "Energy Tips",
   description: "Helpful energy tips to help you save energy.",
@@ -7,24 +9,34 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="flex flex-col space-y-4">
+    <div className="flex flex-col space-y-4 px-4 md:px-0">
       <ComponentLayout>
         <h1 className="text-2xl font-bold text-neutral-800 sm:text-3xl">
           Energy Saving Guide
         </h1>
-        <p className="max-w-xl text-xl text-neutral-800">
+        <p className="max-w-xl text-lg text-neutral-800 sm:text-xl">
           Discover smart ways to reduce your energy consumption and make a
           positive impact on the environment while benefitting your community.
         </p>
       </ComponentLayout>
-      <div className="relative h-[400px] w-full rounded-xl shadow-lg sm:aspect-video sm:h-auto sm:overflow-hidden">
+
+      <div className="relative h-[500px] w-full overflow-hidden rounded-xl bg-neutral-50 shadow-lg sm:aspect-video sm:h-auto">
+        <div></div>
         <iframe
           src="https://octopus.energy/blog/energy-saving-tips/"
-          className="absolute left-0 top-0 h-full w-full border-0"
+          className="absolute left-0 top-0 h-full w-full rounded-xl border-0"
           title="Energy Saving Tips"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         />
       </div>
+      <Link
+        href="https://octopus.energy/blog/energy-saving-tips/"
+        target="_blank"
+      >
+        <ComponentLayout className=" bg-indigo-100 hover:bg-indigo-200  sm:hidden">
+          Open the Guide in New Tab
+        </ComponentLayout>
+      </Link>
     </div>
   );
 }
